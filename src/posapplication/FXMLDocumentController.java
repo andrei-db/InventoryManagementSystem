9790cf4c2cce959395a13f8e04fbb4a61951949f
AndreiDaraban <72549078+andrei-db@users.noreply.github.com>
2023-02-23 11:30:00 +0200

    Created Login UI

--- src/posapplication/FXMLDocumentController.java
+++ src/posapplication/FXMLDocumentController.java
@@ -9,7 +9,11 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -17,13 +21,23 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
+  @FXML
+    private Button closeBtn;
+
     @FXML
-    private Label label;
+    private Button loginBtn;
+
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField usernameField;
     
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    public void close(){
+        System.exit(0);
     }
     
     @Override
