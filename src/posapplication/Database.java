4edba37e97fc25a6b253ba38e420e5e3798f61aa
AndreiDaraban <72549078+andrei-db@users.noreply.github.com>
2023-02-23 14:22:41 +0200

    Dashboard UI for Home page
    -redirected to dashboard if admin credentials matched
    -created UI for dashboard file

--- src/posapplication/Database.java
+++ src/posapplication/Database.java
@@ -5,19 +5,28 @@
 package posapplication;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author andreidb
  */
 public class Database {
-    public static Connection connectDb(){
-        try{
-            Class.forName("com.mysql.jdbc.Driver")
+
+    public static Connection connectDb() {
+
+        Connection connect;
+        try {
+            connect = DriverManager.getConnection("jdbc:mysql://localhost/pos_inventory", "andreidb", "12345");
+            return connect;
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
-        catch(Exception e){
-            e.printStackTrace();
-        }
+
+        return null;
     }
-    
+
 }
--- src/posapplication/FXMLDocumentController.java
+++ src/posapplication/FXMLDocumentController.java
@@ -4,27 +4,38 @@
  */
 package posapplication;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
  * @author andreidb
  */
 public class FXMLDocumentController implements Initializable {
-    
-  @FXML
+
+    @FXML
     private Button closeBtn;
 
     @FXML
@@ -38,21 +49,75 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private TextField usernameField;
+
+    @FXML
+    private Label usernameLabel;
     
     private Connection connect;
     private PreparedStatement prepare;
-    private ResultSet resultSet;
-    
-    public void LoginAdmin(){
+    private ResultSet result;
+
+    public void LoginAdmin() {
+        String sql = "SELECT * FROM admin WHERE username=? and password=?";
+        connect = Database.connectDb();
+        try {
+            prepare = connect.prepareStatement(sql);
+            prepare.setString(1, usernameField.getText());
+            prepare.setString(2, passwordField.getText());
+
+            result = prepare.executeQuery();
+         
+            Alert alert;
+            if (usernameField.getText().isEmpty() || passwordField.getText().isEmpty()) {
+                
+                alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Please fill all blank fields");
+                alert.showAndWait();
+            } else {
+                if (result.next()) {
+                    alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully Login!");
+                    alert.showAndWait();
+                    loginBtn.getScene().getWindow().hide();
+                    
+                    Parent root;
+              
+                    try {
+                        root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+                        
+                        Stage stage = new Stage();
+                        stage.initStyle(StageStyle.TRANSPARENT);
+                        Scene scene = new Scene(root);
+                        stage.setScene(scene);
+                        stage.show();
+                    } catch (IOException ex) {
+                        Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                } else {
+                    alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Wrong Username/Password");
+                    alert.showAndWait();
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
-    public void Close(){
+
+    public void Close() {
         System.exit(0);
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
